a06ea01354b79721b8fd863d9128f6429e96fa47
wadawson <wadawson@ucdavis.edu>
2013-08-14 10:57:50 -0700

    performed a centroid uncertainty run based on probabilistic bootstrap sampling
    I don't really trust these results due to the low purity of the cluster sample in the
    bootstrap draws.  This shows a very high confidence in the galaxy-dm offset but
    again I am skeptical of the results.
    
    Note that the north centroid result figure are no good since they were not made
    from the customized north density fits file.

---
+++ Chapter4/galaxyDenMapInput_24maglim_v12speczmod.py
@@ -0,0 +1,37 @@
+'''
+Input file for the creation of galaxy number density maps for the DLS and 
+Med-band photo-z catalogs.
+'''
+from CAT_v12_speczmod2 import numberdensity
+
+catalog= '/Users/dawson/SkyDrive/Research/Clusters/DLSCL09162953/GalaxyDensity/specz_photoz_based/with2012bRedshifts/MedPerrysClusterCat_2013specz.txt'
+ra_ttype = 'alpha'
+dec_ttype = 'delta'
+spec_z_ttype = 'z_spec'
+photo_z_ttype = 'z_mid'
+photoz_errorfactor = 0.07
+
+rabin=200
+N_boot = 10000
+magcol = 'R'
+magrange = (None,24)
+rarange=(138.99750,139.09361)
+decrange=(29.79978,29.88314)
+
+# South Subcluster
+# note that I have to customize this for the north and south subclusters
+z_cluster = 0.53414
+vdisp_cluster = 770 #km/s
+prefix= 'BVRzMed_24maglim_south_10000'
+numberdensity(catalog, ra_ttype, dec_ttype, spec_z_ttype, photo_z_ttype, 
+              photoz_errorfactor, rabin, prefix, z_cluster, vdisp_cluster, 
+              rarange, decrange, N_boot, magcol ,magrange)
+
+# North Subcluster
+# note that I have to customize this for the north and south subclusters
+z_cluster = 0.53074
+vdisp_cluster = 740 #km/s
+prefix= 'BVRzMed_24maglim_north_10000'
+numberdensity(catalog, ra_ttype, dec_ttype, spec_z_ttype, photo_z_ttype, 
+              photoz_errorfactor, rabin, prefix, z_cluster, vdisp_cluster, 
+              rarange, decrange, N_boot, magcol ,magrange )
---
+++ Chapter4/input_CenterVarianceBoot10000.py
@@ -0,0 +1,35 @@
+'''
+This is an input script for running CenterVarianceBoot.py
+'''
+from CenterVarianceBoot import *
+
+# Galaxy Density Input
+# User inputs:
+id_sci = 0
+N_boot = 10000
+id_boot = 3
+
+r_step = 1
+r_stop = 21 #~200 kpc
+N_bins = 50 #number of bins or histogram plots
+
+
+# South Subcluster
+fitsfile = '/Users/dawson/Git/dissertation/Chapter4/BVRzMed_24maglim_south_10000_numberdensity.fits'
+x_start = 148
+y_start = 50
+r_start = 40
+prefix = 'galden_10000_south'
+
+cent_n_var(fitsfile,id_sci,N_boot,id_boot,x_start,y_start,r_start,r_step,r_stop,N_bins,prefix)
+
+# North Subcluster
+fitsfile = '/Users/dawson/Git/dissertation/Chapter4/BVRzMed_24maglim_north_10000_numberdensity.fits'
+x_start = 104
+y_start = 163
+r_start = 63
+prefix = 'galden_10000_north'
+
+cent_n_var(fitsfile,id_sci,N_boot,id_boot,x_start,y_start,r_start,r_step,r_stop,N_bins,prefix)
+
+pylab.show()
